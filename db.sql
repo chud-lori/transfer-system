7e5ddb7aae6701d618ba10c6b6fa0e350ba39ac7
chud-lori <imlori000@gmail.com>
2025-06-10 00:59:08 +0700

    update db constraint check

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
 create table accounts (
     id integer primary key,
-    balance NUMERIC(20, 5) NOT NULL DEFAULT 0.00000,
+    balance NUMERIC(20, 5) NOT NULL DEFAULT 0.00000 CONSTRAINT positive_balance CHECK (balance >= 0),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -8,6 +8,6 @@ CREATE TABLE transactions (
     id serial primary key,
     source_id integer not null references accounts(id),
     destination_id integer not null references accounts(id),
-    amount NUMERIC(20, 5) NOT NULL DEFAULT 0.00000,
+    amount NUMERIC(20, 5) NOT NULL DEFAULT 0.00000 CONSTRAINT min_amount CHECK (amount > 0),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );

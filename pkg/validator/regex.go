8d4b58d890d7c8437ec6963c3fcdd5e91af05a05
chud-lori <imlori000@gmail.com>
2025-06-10 15:22:04 +0700

    update regex to be strict

--- pkg/validator/regex.go
+++ pkg/validator/regex.go
@@ -5,6 +5,6 @@ import "regexp"
 func ValidateDecimalFormat(input string) bool {
 	// assuming that the input will always in number with 5 digits after the decimal point
 	// e.g. 12345.12345
-	regex := regexp.MustCompile(`^\d+(\.\d{5})?$`)
+	regex := regexp.MustCompile(`^\d+\.\d{5}$`)
 	return regex.MatchString(input)
 }
